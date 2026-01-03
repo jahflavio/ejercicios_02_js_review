45ae1e200d6f652daed7af4a06b82247223b855b
fabianfloram <fabianfloram@gmil.com>
2026-01-03 12:30:34 -0600

    Refactor asynchronous JavaScript examples to use async/await syntax

--- script.js
+++ script.js
@@ -307,10 +307,22 @@ booksAfterUpdate;
 */
 
 //ASYNCHRONOUS js: PROMISES
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+//fetch('https://jsonplaceholder.typicode.com/todos')
+//  .then((res) => res.json())
+//  .then((data) => console.log(data));
 
-console.log('jonas');
+//console.log('jonas');
 
 //ASYNCHRONOUS js: ASYNC/AWAIT
+async function getTodos() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
+console.log('jonas');
